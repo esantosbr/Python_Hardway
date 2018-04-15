d8ddddc02e2a755ce3e78907de46f3b3debfe38c
esantosbr <esantos@actar.com.br>
2018-04-15 16:44:19 -0300

    Inserido comentário.

--- ex11.py
+++ ex11.py
@@ -1,4 +1,8 @@
+# Make a question and don't end the line with a new line. end=''
 print("How old are you? ", end='')
+# Insert into variable 'age' a string received from user.
+# input() is a function that receives a string from user.
+# If you need another type of value you need to convert it before. 
 age = input()
 print("How tall are you? ", end='')
 height = input()
