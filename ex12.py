a1531db78838aad1ec4957ed9f751aefcb3bcbb7
esantosbr <esantos@actar.com.br>
2018-04-15 17:44:59 -0300

    Inserido comentário.

--- ex12.py
+++ ex12.py
@@ -1,4 +1,8 @@
+# This is an easy way to use input() function.
+# This way only value entered from the user will be into age variable
 age = input("How old are you? ")
+# If user insert 38 when asked for age and after use print(age)
+# the printed value will be only 38 and not the string "How old are you? 38"
 height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
