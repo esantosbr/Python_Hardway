6fd81f8a993d76acdd014cd0e9d1fc603b602573
esantosbr <esantos@actar.com.br>
2018-04-15 18:15:24 -0300

    Adicionado comentário.

--- ex13.py
+++ ex13.py
@@ -1,5 +1,12 @@
+# importing module argv from sys to use in this code
 from sys import argv
 # read the WYSS section for how to run This
+"""
+The following line says:
+"Take whatever is in argv, unpack it,
+and assign it to all of these variables
+on the left in order."
+"""
 script, first, second, third = argv
 
 print("The script is called:", script)
