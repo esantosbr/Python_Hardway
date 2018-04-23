0502f4905274dc173e0469e3086e64ee05ba6f97
esantosbr <esantos@actar.com.br>
2018-04-23 15:00:27 -0300

    Study Drills. Use input instead of argv to open file.

--- ex15.py
+++ ex15.py
@@ -1,4 +1,5 @@
 #import argv from sys module
+"""
 from sys import argv
 #Define variables to use argv
 script, filename = argv
@@ -8,12 +9,11 @@ txt = open(filename)
 print(f"Here's your file {filename}:")
 #Print content readed from file defined in txt variable
 print(txt.read())
-#Asks the user to re-enter the file name
 """
+#Asks the user to re-enter the file name
 print(f"Type the filename again:")
 file_again = input(">")
 #Open file name entered from user and return a stream to txt variable.
 txt_again = open(file_again)
 #Print content readed from file defined in txt_again variable
 print(txt_again.read())
-"""
