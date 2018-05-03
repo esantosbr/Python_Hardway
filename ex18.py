afef6085574cf579ebe6f2d76ae19051ee062261
esantosbr <esantos@actar.com.br>
2018-05-03 15:39:05 -0300

    Exercício 18

--- ex18.py
+++ ex18.py
@@ -4,7 +4,7 @@ def print_two(*args):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
     # ok, that *args is actually pointless, we can just do this
-    def print_two_again(arg1, arg2):
+def print_two_again(arg1, arg2):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
     # this just takes one argument
@@ -12,11 +12,11 @@ def print_two(*args):
         print(f"arg1: {arg1}")
 
     # this one takes no arguments
-    def print_none():
-        print("I got nothin'.")
+def print_none():
+    print("I got nothin'.")
 
 
-        print_two("Zed","Shaw")
-        print_two_again("Zed","Shaw")
-        print_one("First")
-        print_none()
+    print_two("Zed","Shaw")
+    print_two_again("Zed","Shaw")
+    print_one("First")
+    print_none()
