69c1cd25d5e6f55e933123dc95e69085f7787b72
esantosbr <esantos@actar.com.br>
2018-05-23 19:13:59 -0300

    Alterado o modo de importação do arquivo ex_functions e do uso de suas funções.

--- ex25.py
+++ ex25.py
@@ -1,16 +1,16 @@
-import ex25_functions
+from ex25_functions import *
 sentence = "All good things come to those who wait."
-words = ex25_functions.break_words(sentence)
+words = break_words(sentence)
 words
-sorted_words = ex25_functions.sort_words(words)
+sorted_words = sort_words(words)
 sorted_words
-ex25_functions.print_first_word(words)
-ex25_functions.print_last_word(words)
+print_first_word(words)
+print_last_word(words)
 words
-ex25_functions.print_first_word(sorted_words)
-ex25_functions.print_last_word(sorted_words)
+print_first_word(sorted_words)
+print_last_word(sorted_words)
 sorted_words
-sorted_words = ex25_functions.sort_sentence(sentence)
+sorted_words = sort_sentence(sentence)
 sorted_words
-ex25_functions.print_first_and_last(sentence)
-ex25_functions.print_first_and_last_sorted(sentence)
+print_first_and_last(sentence)
+print_first_and_last_sorted(sentence)
